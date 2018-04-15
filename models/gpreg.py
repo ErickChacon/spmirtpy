fe1420c979eb142906b80cb29d2ae38ea24f5cd3
Erick Chacon Montalvan <eral.th07@gmail.com>
2018-04-15 13:31:09 +0100

    much faster algorithm 2x

--- models/gpreg.py
+++ models/gpreg.py
@@ -21,8 +21,7 @@ def gpreg(y, dist, params, niter, Sigma_proposal):
 
     # Parameters
     params_log = tf.Variable(params, dtype = tf.float32)
-    sigma2 = tf.exp(params_log[0,0])
-    phi = tf.exp(params_log[1,0])
+    params_log_first = tf.Variable(params, dtype = tf.float32)
 
     # Data
     tf_dis = tf.placeholder(dtype = tf.float32)
@@ -33,25 +32,30 @@ def gpreg(y, dist, params, niter, Sigma_proposal):
             }
 
     # Computation
-    Sigma_gp = tf_cov_exp(tf_dis, sigma2, phi, 0.0)
-    Sigma_marginal = prior_c_sigma2 + Sigma_gp
-    Sigma_z = Sigma_marginal + tf.eye(n)
+    # params_log_posterior = posterior(params_log_first, tf_dis, tf_y, prior_c_sigma2)
+
+    sess_init = tf.Session()
+    sess_init.run(tf.global_variables_initializer())
+    plop = sess_init.run(posterior(params_log, tf_dis, tf_y, prior_c_sigma2),
+            feed_dict)
+    params_log_posterior = tf.Variable(plop, dtype = tf.float32)
 
     # Metropolis Hastings sampler
-    updater = update(params_log, Sigma_proposal_chol, tf_dis, Sigma_z,
-            tf_y, prior_c_sigma2)
+    updater = update(params_log, params_log_posterior,
+            Sigma_proposal_chol, tf_dis, tf_y, prior_c_sigma2)
 
     # Iterate trough MH sampler
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
     for i in range(niter):
-        params_new = sess.run(updater, feed_dict)
+        params_new, params_log_posterior_new = sess.run(updater, feed_dict)
         samples[i, :] = np.reshape(np.exp(params_new), 2)
+        # print(sess.run(params_log_posterior, feed_dict))
 
     sess.run(updater, feed_dict)
     return samples
 
-def update(params_log, Sigma_proposal_chol, tf_dis, Sigma_z, tf_y,
+def update(params_log, params_log_posterior, Sigma_proposal_chol, tf_dis, tf_y,
         prior_c_sigma2):
     """TODO: Docstring for update.
 
@@ -64,24 +68,42 @@ def update(params_log, Sigma_proposal_chol, tf_dis, Sigma_z, tf_y,
     params_log_aux = params_log + \
             tf.matmul(Sigma_proposal_chol,
                     tf.distributions.Normal(0.0, 1.0).sample(tf.shape(params_log)))
-    sigma2_aux = tf.exp(params_log_aux[0,0])
-    phi_aux = tf.exp(params_log_aux[1,0])
 
-    Sigma_gp_aux = tf_cov_exp(tf_dis, sigma2_aux, phi_aux, 0.0)
-    Sigma_marginal_aux = prior_c_sigma2 + Sigma_gp_aux
-    Sigma_z_aux = Sigma_marginal_aux + tf.eye(n)
-    acceptance_prob_log = dmvnorm(tf_y, zeros_n, Sigma_z_aux) + \
-            distr.Normal(tf.log(1.0), 0.4).log_prob(params_log_aux[0,0]) + \
-            distr.Normal(tf.log(0.08), 0.4).log_prob(params_log_aux[1,0]) - \
-            dmvnorm(tf_y, zeros_n, Sigma_z) - \
-            distr.Normal(tf.log(1.0), 0.4).log_prob(params_log[0,0]) - \
-            distr.Normal(tf.log(0.08), 0.4).log_prob(params_log[1,0])
+    params_log_aux_posterior = posterior(params_log_aux, tf_dis, tf_y, prior_c_sigma2)
+    acceptance_prob_log = params_log_aux_posterior - params_log_posterior
 
     uniform_log = tf.log(tf.distributions.Uniform().sample())
     params_log_new = tf.cond(tf.greater(acceptance_prob_log,uniform_log),\
             lambda: params_log_aux, lambda: params_log)
+    params_log_posterior_new = tf.cond(tf.greater(acceptance_prob_log,uniform_log),\
+            lambda: params_log_aux_posterior, lambda: params_log_posterior)
 
-    return tf.assign(params_log, params_log_new)
+    op1 = tf.assign(params_log, params_log_new)
+    op2 = tf.assign(params_log_posterior, params_log_posterior_new)
+    return op1, op2
+    # return params_log_new
+
+def posterior(params_log, tf_dis, tf_y, prior_c_sigma2):
+    """TODO: Docstring for posterior.
+
+    :params_log: TODO
+    :tf_dis: TODO
+    :tf: TODO
+    :returns: TODO
+
+    """
+    n = tf.size(tf_y)
+    zeros_n = tf.zeros([n, 1])
+    sigma2 = tf.exp(params_log[0,0])
+    phi = tf.exp(params_log[1,0])
+
+    Sigma_gp = tf_cov_exp(tf_dis, sigma2, phi, 0.0)
+    Sigma_marginal = prior_c_sigma2 + Sigma_gp
+    Sigma_z = Sigma_marginal + tf.eye(n)
+    posterior_prob_log = dmvnorm(tf_y, zeros_n, Sigma_z) + \
+            distr.Normal(tf.log(1.0), 0.4).log_prob(params_log[0,0]) + \
+            distr.Normal(tf.log(0.08), 0.4).log_prob(params_log[1,0])
+    return posterior_prob_log
 
 
 def dmvnorm(y, mean, sigma):
@@ -97,15 +119,31 @@ def tf_cov_exp(d, sigma2, phi, nugget):
     S = sigma2 * tf.exp(- d / phi) + nugget * tf.eye(tf.shape(d)[1])
     return(S)
 
-def test():
+def test(params, dist, y):
     """TODO: Docstring for test.
 
     :x: TODO
     :returns: TODO
 
     """
+    # Data
+    tf_dis = tf.placeholder(dtype = tf.float32)
+    tf_y = tf.placeholder(dtype = tf.float32)
+    feed_dict = {
+            tf_dis: dist,
+            tf_y: y
+            }
 
-    sess0 = tf.Session()
-    bla = tf.ones(10) - tf.ones([10,1])
-    return sess0.run(bla)
+    prior_c_sigma2 = tf.constant(1.0)
+    params_log = tf.Variable(params, dtype = tf.float32)
+
+    sess_init = tf.Session()
+    sess_init.run(tf.global_variables_initializer())
+    plop = sess_init.run(posterior(params_log, tf_dis, tf_y, prior_c_sigma2),
+            feed_dict)
+    return plop
+
+    # sess0 = tf.Session()
+    # bla = tf.ones(10) - tf.ones([10,1])
+    # return sess0.run(bla)
 
